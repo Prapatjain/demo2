8ac9e2029d85ba395f7d9fdb1857afa3adeae565
Prapatjain <prapatjain@gmail.com>
2025-01-20 14:59:51 +0530

    deleted

--- primerange.java
+++
@@ -1,9 +0,0 @@
-class primerange{
-	public static void main(String[] args){
-		// range of prime no. b/w 1 - 10
-		System.out.println(2);
-		System.out.println(3);
-		System.out.println(5);
-		System.out.println(7);
-	}
-}
